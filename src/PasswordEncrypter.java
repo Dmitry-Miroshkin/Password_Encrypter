f00a87f63e67b865b752bf0b30ea8e2524fc512d
Dmitry-Miroshkin <dmm@mail.ru>
2021-11-22 23:56:56 +0300

    Encrypt and encode added algorithm selection

--- src/PasswordEncrypter.java
+++ src/PasswordEncrypter.java
@@ -4,17 +4,28 @@ import java.util.Base64;
 
 public class PasswordEncrypter {
     //зашифрованный пароль
-    public static byte[] encrypt(String password) {
+    // можно указать не только пароль но и алгоритм шифрования
+
+    public static byte[] encrypt(String password, String algorithm) throws IllegalArgumentException {
         try {
-            MessageDigest d = MessageDigest.getInstance("sha-1");
+            MessageDigest d = MessageDigest.getInstance(algorithm);
             d.update(password.getBytes());
             return d.digest();
         } catch (NoSuchAlgorithmException nsae) {
+            throw new IllegalArgumentException("Illegal algorithm value.");
         }
-        return null;
     }
-    public static  String encryptAndEncode(String password){
-        String encoded = Base64.getEncoder().encodeToString(encrypt(password));
-        return encoded;
+
+    // упрощенный вызов без  указания алгоритма
+    public static byte[] encrypt(String password) throws IllegalArgumentException {
+        return encrypt(password, "sha-1");
+    }
+
+    public static String encryptAndEncode(String password, String algorithm) {
+        return Base64.getEncoder().encodeToString(encrypt(password, algorithm));
+    }
+    // упрощенная версия для формирования хэш- ключа по алгоритму sha-1
+    public static String encryptAndEncode(String password) throws IllegalArgumentException {
+        return encryptAndEncode(password, "sha-1");
     }
 }
